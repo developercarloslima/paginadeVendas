bb7c0e6bf04ccdb0a335e458bafbb572b8c6d829
Carlos <developercarloslima@gmail.com>
2026-05-04 17:05:15 -0300

    organizando a responsividade e ajustando o JS

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 let index = 0;
 const slides = document.querySelectorAll(".carousel img");
 
+const isMobile = () => window.innerWidth <= 768;
+
 const showSlide = () => {
   slides.forEach(s => s.classList.remove("active"));
 
-  const first = index;
-  const second = (index + 1) % slides.length;
+  // sempre mostra a atual
+  slides[index].classList.add("active");
 
-  slides[first].classList.add("active");
-  slides[second].classList.add("active");
+  // desktop mostra 2
+  if (!isMobile()) {
+    const second = (index + 1) % slides.length;
+    slides[second].classList.add("active");
+  }
 
   index = (index + 1) % slides.length;
 };
 
-
+// autoplay
 setInterval(showSlide, 3000);
+
+// inicia
 showSlide();
 
-// BOTÃO
+// atualiza se mudar tamanho da tela
+window.addEventListener("resize", showSlide);
+
+/* BOTÃO */
 document.querySelectorAll(".btn").forEach(btn => {
   btn.onclick = () => {
-    window.location.href = "https://pay.cakto.com.br/3fk9ybd_860619"; // coloca checkout aqui
+    window.location.href = "https://pay.cakto.com.br/3fk9ybd_860619";
   };
 });
